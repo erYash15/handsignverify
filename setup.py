e1b19fea45651069eb47208985f5655bc01311fc
eryash15 <eryash15@gmail.com>
2021-12-29 19:32:39 +0530

    0.0.1

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url='https://github.com/eryash15/handsignverify',
     author='Yash Gupta',
     author_email='eryash15@gmail.com',
+    download_url = 'https://github.com/erYash15/handsignverify/archive/refs/tags/handsignverify.tar.gz',
     license='MIT',
     keywords=['keras','signature','deeplearning'],
     install_requires=['numpy>=1.9.1', 'scipy>=0.14', 'h5py', 'pillow', 'keras','six>=1.9.0', 'pyyaml']
