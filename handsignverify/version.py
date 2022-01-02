bafeda6a0997951dca6e1da7575caa91ad33a5c8
Yash Gupta <34357926+erYash15@users.noreply.github.com>
2022-01-02 12:37:38 +0530

    Delete version.py

--- handsignverify/version.py
+++
@@ -1,8 +0,0 @@
-__version__ = '0.1'
-
-def pretty_versions():
-    import keras
-    import tensorflow as tf
-    k_version = keras.__version__
-    t_version = tf.__version__
-    return "keras-vggface : {}, keras : {} , tensorflow : {} ".format(__version__,k_version,t_version)
